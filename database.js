b0283371a95f0d1590ee95a7f4967ebdaf9db921
Kelly Gasasira Musonera <kgasasira69@gmail.com>
2019-07-08 12:50:32 +0200

    restractured database connection

---
+++ database.js
@@ -0,0 +1,3 @@
+const database = 'mongodb+srv://kgasasira:1991sexybrown@mongodb-kelly-atztv.mongodb.net/test?retryWrites=true&w=majority'
+
+module.exports = database;
--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const database = require('../database');
 
-const db = mongoose.connect('mongodb+srv://kgasasira:1991sexybrown@mongodb-kelly-atztv.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser:true})
+const db = mongodb.MongoClient.connect(database, {useNewUrlParser:true})
             .then(() => console.log('\x1b[34m%s\x1b[0m','MongoDB is connected........'))
             .catch((err) => console.log('\x1b[31m%s\x1b[0m', err));
 

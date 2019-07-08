4c7b4e5ff138658299d24aef92fbe7ef8cff11a1
Kelly Gasasira Musonera <kgasasira69@gmail.com>
2019-07-08 12:14:37 +0200

    database for mlab

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost/banka', {useNewUrlParser:true})
+const db = mongoose.connect('mongodb+srv://kgasasira:1991sexybrown@mongodb-kelly-atztv.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser:true})
             .then(() => console.log('\x1b[34m%s\x1b[0m','MongoDB is connected........'))
             .catch((err) => console.log('\x1b[31m%s\x1b[0m', err));
 

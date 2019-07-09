30d1b6708a14727e9241aa2eaf3e6958edfbc7e5
Kelly Gasasira Musonera <kgasasira69@gmail.com>
2019-07-09 10:58:18 +0200

    secretKey

--- src/db.js
+++ src/db.js
@@ -4,5 +4,4 @@ const db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true})
             .then(() => console.log('\x1b[34m%s\x1b[0m','MongoDB is connected........'))
             .catch((err) => console.log('\x1b[31m%s\x1b[0m', err));
 
-
 module.exports = db;

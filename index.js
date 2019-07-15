702066f1114a669675a611b740241afba97d65ea
Kelly Gasasira Musonera <kgasasira69@gmail.com>
2019-07-15 16:06:44 +0200

    morgan

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ require('./src/db');
 const client = require('./routes/users/clientRt');
 const transaction = require('./routes/transactionRt');
 const staff = require('./routes/users/staffRt');
+const morgan = require('morgan')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(morgan('tiny'));
 app.use('/api/v1/users', client);
 app.use('/api/v1/transactions',transaction);
 app.use('/api/v1/admin', staff);
